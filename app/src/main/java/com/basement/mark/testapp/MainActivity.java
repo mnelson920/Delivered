980cec45027fb458266c3fa1b5ff53eaaaf6742b
mnelson920 <markn920dev@gmail.com>
2017-05-04 07:05:56 -0400

    Delete commented code

--- app/src/main/java/com/basement/mark/testapp/MainActivity.java
+++ app/src/main/java/com/basement/mark/testapp/MainActivity.java
@@ -280,15 +280,4 @@ public class MainActivity extends Activity {
         }
     };
 
-
-
-   /* public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    } */
-
-
 }
